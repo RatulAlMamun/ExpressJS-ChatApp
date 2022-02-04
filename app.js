0aa6024a00ddeb5b0737d6233d7f6f30ed6b015e
RatulAlMamun <ratulalmamun23@gmail.com>
2022-02-04 23:53:56 +0600

    project setup done

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log;
+// dependencies
+const path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+// intiating express object
+const app = express();
+
+// dotenv package configuration
+dotenv.config();
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successfull."))
+  .catch((err) => console.log(err));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+
+// error handling
+
+// port setup
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
