2f4c41866f77c010077fe1572a9f10a8f29ac71e
Ratul Al Mamun <ratulalmamun23@gmail.com>
2022-02-06 14:19:22 +0600

    add people model

---
+++ models/People.js
@@ -0,0 +1,42 @@
+// dependencies
+const mongoose = require("mongoose");
+
+// People Schema
+const peopleSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// creating model for People
+const People = mongoose.model("People", peopleSchema);
+
+// exports module
+module.exports = People;
