266c6a44bcc96d1aac93953ea0b8f1f86e5c3eca
Ratul Al Mamun <ratulalmamun23@gmail.com>
2022-03-30 13:11:32 +0600

    Login validation implemented.

---
+++ middleware/login/loginValidator.js
@@ -0,0 +1,37 @@
+// dependencies
+const { check } = require("express-validator");
+
+// login validator logic
+const doLoginValidators = [
+  check("username").isLength({ min: 1 }).withMessage("username is required."),
+  check("password").isLength({ min: 1 }.withMessage("password required.")),
+];
+
+// validation handler
+const doLoginValidationHandler = (req, res, next) => {
+  // fetch the error
+  const error = validationResult(req);
+
+  // mapped the errors with specific format
+  const mappedError = error.mapped();
+
+  //check if errors
+  if (Object.keys(mappedError).length === 0) {
+    next();
+  } else {
+    res.render("index", {
+      data: { username: req.body.username },
+      errors: {
+        common: {
+          msg: error.message,
+        },
+      },
+    });
+  }
+};
+
+// export module
+module.exports = { 
+  doLoginValidators,
+  doLoginValidationHandler
+};
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,17 +2,30 @@
 const express = require("express");
 const { getLogin, login } = require("../controller/loginController");
 const {
+  doLoginValidators,
+  doLoginValidationHandler
+} = require("../middleware/login/loginValidator");
+const {
   decorateHtmlResponse,
 } = require("../middleware/common/decorateHtmlResponse");
 
 // creating router instances for route setup
 const router = express.Router();
 
+// set page title
+const pageTitle = "Login";
+
 // route
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(pageTitle), getLogin);
 
 // process login
-router.post("/", login);
+router.post(
+  "/",
+  decorateHtmlResponse(pageTitle),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
 
 // export routes
 module.exports = router;
