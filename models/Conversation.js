361fa0fbd63eb6d9d003e3d0a85c8dfdada8e4ef
RatulAlMamun <ratulalmamun23@gmail.com>
2022-04-15 11:30:59 +0600

    creating conversation model.

---
+++ models/Conversation.js
@@ -0,0 +1,28 @@
+// dependencies
+const mongoose = require("mongoose");
+
+// Conversation Schema
+const conversationSchema = mongoose.Schema(
+  {
+    creator: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    participant: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    last_update: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// creating model for conversation
+const Conversation = mongoose.model("Conversation", conversationSchema);
